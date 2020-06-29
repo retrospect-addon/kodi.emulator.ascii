e3f912787554d2e235454d9f61f250fd14ca5911
Michaël Arnauts <michael.arnauts@destiny.be>
2020-06-29 12:53:58 +0200

    Fix encoding of special characters in ListItems

--- xbmcgui.py
+++ xbmcgui.py
@@ -338,8 +338,12 @@ class ListItem(KodiStub):
             for key in keys:
                 value = "%s    - %s: %s\n" % (value, key, self.__properties[key])
 
+            if self.PY2:
+                return value.encode('utf-8') if type(value) == unicode else value
             return value
         else:
+            if self.PY2:
+                return self.__label.encode('utf-8') if type(self.__label) == unicode else self.__label
             return self.__label
 
     def __unicode__(self):
