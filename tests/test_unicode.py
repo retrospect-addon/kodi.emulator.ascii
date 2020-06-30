c2dfeb465fdf34b1673839f2fa3eaba8846265bb
Michaël Arnauts <michael.arnauts@destiny.be>
2020-06-30 08:43:12 +0200

    Let's just make UTF-8 the default

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -10,5 +10,7 @@ class TestUnicode(unittest.TestCase):
 
     def test_unicode(self):
         for label in ['saké', u'saké', '酒', u'酒']:
-            li = xbmcgui.ListItem(label=label, path='plugin://plugin.video.example/test')
+            li = xbmcgui.ListItem(label=label, path='plugin://plugin.video.example/' + label)
             xbmcplugin.addDirectoryItem(-1, li.getPath(), li)
+
+        xbmcplugin.endOfDirectory(-1)
--- xbmcgui.py
+++ xbmcgui.py
@@ -338,12 +338,8 @@ class ListItem(KodiStub):
             for key in keys:
                 value = "%s    - %s: %s\n" % (value, key, self.__properties[key])
 
-            if self.PY2:
-                return value.encode('utf-8') if isinstance(value, unicode) else value
             return value
         else:
-            if self.PY2:
-                return self.__label.encode('utf-8') if isinstance(self.__label, unicode) else self.__label
             return self.__label
 
     def __unicode__(self):
