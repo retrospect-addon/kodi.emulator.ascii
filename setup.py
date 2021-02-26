037e4d0f4c7282153daa856622c14ae45aeeadd5
Bas Rieter <basrieter@gmail.com>
2021-02-26 16:56:55 +0100

    Version: sake-0.0.15

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name='sakee',
-    version="0.0.14",
+    version="0.0.15",
     url='https://github.com/retrospect-addon/kodi.emulator.ascii',
     author='Bas Rieter',
     description='SAKÉ can help you to debug and develop Kodi Python add-ons',
